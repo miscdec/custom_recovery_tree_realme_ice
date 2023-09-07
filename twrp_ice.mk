f5f6dee7392f72811ba8ad2d90b98a8b58ab96b5
Carlo Dee <carlodee.official@proton.me>
2023-09-08 06:51:46 +0800

    ice: enable virtual ab to match stock.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- twrp_ice.mk
+++ twrp_ice.mk
@@ -9,6 +9,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inheri virtual A/B OTA stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
